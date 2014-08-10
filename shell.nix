8c07905a08668c66b0d811b3ab05227503166be3
Austin Seipp <austin@well-typed.com>
2014-08-09 21:23:17 -0500

    Add shell.nix
    Signed-off-by: Austin Seipp <austin@well-typed.com>

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  pkgs   = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+
+  env = with pkgs; rec {
+    libedsignEnv = stdenv.mkDerivation rec {
+      name = "libedsign-env";
+      src  = ./.;
+      buildInputs =
+        [ git xz clang gcc framac
+	  perl perlPackages.ListMoreUtils
+	];
+    };
+  };
+in env
